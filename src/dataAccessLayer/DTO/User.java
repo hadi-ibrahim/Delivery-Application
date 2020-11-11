c0023263c6e4628bcfa9806608893a02e69ef3b8
DAVID\david <david_ghoul@hotmail.com>
2020-11-11 21:15:51 +0200

    Updated DTO.User

--- src/dataAccessLayer/DTO/User.java
+++ src/dataAccessLayer/DTO/User.java
@@ -1,5 +1,87 @@
 package dataAccessLayer.DTO;
 
+import java.awt.geom.Point2D;
+
 public class User {
-	
+	private int id;
+	private String firstname;
+	private String lastname;
+	private int age;
+	private int email;
+	private String role;
+	private int phone;
+	private String status;
+	private Point2D location;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getEmail() {
+		return email;
+	}
+
+	public void setEmail(int email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Point2D getLocation() {
+		return location;
+	}
+
+	public void setLocation(Point2D location) {
+		this.location = location;
+	}
 }
